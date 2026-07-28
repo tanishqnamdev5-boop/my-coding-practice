a98732213f147ce37ca1c6dd3823f5da9cad92b2
tanishqnamdev5-boop <tanishqnamdev5@gmail.com>
2026-07-28 14:16:05 +0530

    Add Area and Perimeter calculation program

---
+++ Area and perimeter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int length=11;
+int breadth=13;
+cout <<"area : " <<length*breadth<<endl;
+cout <<"perimeter : "<<2*(length+breadth)<<endl;
+    return 0;
+}
