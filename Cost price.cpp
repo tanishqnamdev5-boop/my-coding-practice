9fecb5b10152fa71eb9cfa70e41da8698f718910
tanishqnamdev5-boop <tanishqnamdev5@gmail.com>
2026-07-28 14:09:23 +0530

    Update Cost price.cpp

--- Cost price.cpp
+++ Cost price.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int side;
+    cout << "Enter the side of the square: ";
+    cin >> side;
+    char $=7;
+   int area=side*side;
+    int cost = area*7;
+    
+    cout << area << endl;
+    cout << "$" <<cost << endl;
+
+    return 0;
+}
