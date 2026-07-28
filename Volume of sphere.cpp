6d104d3013f7b73ab3ec34024cc6405eebf8060a
tanishqnamdev5-boop <tanishqnamdev5@gmail.com>
2026-07-28 14:05:22 +0530

    Create Volume of sphere.cpp

---
+++ Volume of sphere.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float pi=3.14159;
+float radius=5;
+float volume_of_sphere=(4.0/3.0)*pi*radius*radius*radius;
+cout << "Volume of sphere with radius 5: " << volume_of_sphere << endl;
+return 0;
+}
