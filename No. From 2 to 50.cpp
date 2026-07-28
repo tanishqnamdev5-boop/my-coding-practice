64a7a0ac3fe96e294e9d459643e16826af37fb9c
tanishqnamdev5-boop <tanishqnamdev5@gmail.com>
2026-07-28 13:59:56 +0530

    Create No. From 2 to 50.cpp

---
+++ No. From 2 to 50.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /* for (int i=2; i<=50; i+=2) {
+        cout << i << " ";
+} /*
+    /*for (int i=1; i<=50; i+=2) {
+        cout << i << " ";
+    }*/
+    for (int i=1; i<=50; i++) { //first and last wala method ek hi hai but tareeka alag alag hai...
+            cout << i*2 << " ";
+        
+    }
+    return 0;
+}
